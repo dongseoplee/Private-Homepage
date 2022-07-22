c84a98015f70ebdba1e0bf6cd27274b63af912be
Lee-DongSeop <76763417+dongseoplee@users.noreply.github.com>
2022-07-22 12:09:28 +0900

    spring boot

---
+++ privateHomepage/src/main/java/com/example/privateHomepage/PrivateHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.example.privateHomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateHomepageApplication.class, args);
+	}
+
+}
---
+++ privateHomepage/src/test/java/com/example/privateHomepage/PrivateHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privateHomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
