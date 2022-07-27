3093bf46e99dccc12c0d45bd1b74725ec31a05c3
Lee-DongSeop <76763417+dongseoplee@users.noreply.github.com>
2022-07-28 00:59:28 +0900

    add DTO file

---
+++ privateHomepage/src/main/java/com/example/privateHomepage/dto/sampleDTO.java
@@ -0,0 +1,35 @@
+package com.example.privateHomepage.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Getter
+@Setter
+public class sampleDTO {
+    private Long number;
+    private String user_name;
+    private String university;
+    private String dept;
+    private String birthday;
+    private String location;
+    private String email_address;
+    private String phone_number;
+    private String git_url;
+    private String skill;
+    private String job;
+    private String project1_url;
+    private String project1_info;
+    private String project2_url;
+    private String project2_info;
+    private String project3_url;
+    private String project3_info;
+    private String project4_url;
+    private String project4_info;
+    private String project5_url;
+    private String project5_info;
+
+}
--- privateHomepage/src/main/java/com/example/privateHomepage/repository/SampleRepository.java
+++ privateHomepage/src/main/java/com/example/privateHomepage/repository/SampleRepository.java
@@ -1,13 +1,20 @@
 package com.example.privateHomepage.repository;
 
 import com.example.privateHomepage.domain.Sample;
+import com.example.privateHomepage.dto.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SampleRepository extends JpaRepository<Sample, Long> {
+    //#{entityName} <- 자동으로 entity type을 참고한다. 여기서 entity type은 Sample
+
+    /*@Query("select ")
+    public List<Sample> findAllByUserNumber(Long number);
+     */
 
 } //<sample (클래스 이름), String (pk의 자료형)>
 
