f76faf851b5f5fa95daf615b61a3a9dbeed677fb
Lee-DongSeop <76763417+dongseoplee@users.noreply.github.com>
2022-07-28 01:40:33 +0900

    resolve conflict index.html

--- privateHomepage/src/main/reactfront/src/MainPage/MainPage.js
+++ privateHomepage/src/main/reactfront/src/MainPage/MainPage.js
@@ -16,7 +16,7 @@ const MainPage = () => {
 			.then(res => res.json())
 			.then(data => {
 				console.log(data);
-				setUserInfo(data[0]);
+				setUserInfo(data[0]); //0: lds 1:cgj
 			});
 	}, []);
 
--- privateHomepage/src/main/reactfront/src/NavBar/NavBar.js
+++ privateHomepage/src/main/reactfront/src/NavBar/NavBar.js
@@ -84,7 +84,7 @@ const NavBar = () => {
 		} else if (size.width > 877 && size.width < 1228) {
 			position = 1700;
 		} else if (size.width > 1228) {
-			position = 1980;
+			position = 1840;
 		}
 
 		window.scrollTo({
@@ -104,7 +104,7 @@ const NavBar = () => {
 		fetch(`https://dongsseop2.com/privateHomepage/user`)
 			.then(res => res.json())
 			.then(data => {
-				setUserInfo(data[0]);
+				setUserInfo(data[0]); //0: lds 1:cgj
 			});
 	}, []);
 
